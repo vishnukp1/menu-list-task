80b6bd2ac569f2378545ba56f50a45b8ecc109f1
vishnu KP <vishnuto16@gmail.com>
2024-05-18 12:46:54 +0530

    feat:updated the services adding useffect

--- frontend/src/services/service.jsx
+++ frontend/src/services/service.jsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import Axios from "../api-config/axiosConfig";
 
-export const useGetItems = () => {
-  const getItems = async () => {
-    try {
-      const response = await Axios.get(`/api/items`);
-      return response.data.data;
-    } catch (error) {
-      console.error("Error getting items:", error);
-      throw error;
-    }
-  };
+export const useGetFilteredItems = () => {
+  const [drinks, setDrinks] = useState([]);
+  const [brunchCocktails, setBrunchCocktails] = useState([]);
+  const [error, setError] = useState(null);
 
-  return { getItems };
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await Axios.get(`/api/items`);
+        const data = response.data.data;
+
+        const filteredDrinks = data.filter(item => item.category === 'DRINKS');
+        const filteredBrunchCocktails = data.filter(item => item.category === 'BRUNCH COCKTAILS');
+
+        setDrinks(filteredDrinks);
+        setBrunchCocktails(filteredBrunchCocktails);
+      } catch (error) {
+        console.error("Error getting items:", error);
+        setError(error);
+      }
+    };
+
+    fetchItems();
+  }, []);
+
+  return { drinks, brunchCocktails, error };
 };
