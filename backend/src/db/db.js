f1c3f41907e362f302093a1c72b0e85d9e0d162a
vishnu KP <vishnuto16@gmail.com>
2024-05-23 17:15:11 +0530

    commit

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -5,6 +5,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('Database connected successfully');
